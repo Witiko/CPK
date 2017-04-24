bf609e449339aedc8a30450e81805e28af6f5b19
MartinKravec <kravec.martin@gmail.com>
2017-04-24 15:20:45 +0200

    Prepare template a actions for institutions tree

--- themes/bootstrap3/js/user-settings.js
+++ themes/bootstrap3/js/user-settings.js
@@ -49,4 +49,58 @@ jQuery( document ).ready( function( $ ) {
 			}
 		});
 	});
+
+	/*
+	 * Save chosen institutions to DB
+	 */
+	$( 'body' ).on( 'click', '#save-these-institutions', function( event ) {
+		event.preventDefault();
+
+		var data = {};
+		var institutions = [];
+
+		var selectedInstitutions = $('#facet_institution').jstree(true).get_bottom_selected();
+		$.each( selectedInstitutions, function( index, value ){
+			var explodedArray = value.split(":");
+			institutions.push(explodedArray[1].slice(1, -1));
+		});
+
+		data['institutions'] = institutions;
+
+		$.ajax({
+			type: 'POST',
+			cache: false,
+			dataType: 'json',
+			url: VuFind.getPath() + '/AJAX/JSON?method=saveTheseInstitutions',
+			data: data,
+			beforeSend: function() {
+			},
+			success: function( response ) {
+				console.log( 'Save these institutions: ' );
+				console.log( data );
+				if (response.status == 'OK') {
+
+					$( '#save-these-institutions-confirmation' ).modal( 'show' );
+
+					setTimeout( function() {
+						$( '#save-these-institutions-confirmation' ).modal( 'hide' );
+					}, 1200 );
+
+				} else {
+					console.error(response.data);
+				}
+
+			},
+			error: function ( xmlHttpRequest, status, error ) {
+				$( '#search-results-loader' ).remove();
+				console.error(xmlHttpRequest.responseText);
+				console.error(xmlHttpRequest);
+				console.error(status);
+				console.error(error);
+			},
+			complete: function ( xmlHttpRequest, textStatus ) {
+			}
+		});
+
+	});
 });
